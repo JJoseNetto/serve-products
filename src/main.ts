b12001d8aedb4e37a090eb5468f5ffdc0634dce8
jjose <jjosenetto2020n@gmail.com>
2025-07-19 15:07:59 -0300

    finally commit complet project back-end

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import env from './env';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:5173',
+  });
+
   await app.listen(env.PORT);
 }
 bootstrap();
